55143afc2d0230adb6385341eb58826cf66c6ce2
Warren Falk <warren@warrenfalk.com>
2015-12-28 17:32:25 -0500

    support linux

--- Platform.cs
+++ Platform.cs
@@ -16,10 +16,20 @@ namespace RocksDbSharp
         [DllImport("kernel32", CharSet = CharSet.Auto, BestFitMapping = false, SetLastError = true)]
         private static extern IntPtr LoadLibrary(string fileName);
 
+        [DllImport("libdl")]
+        static extern IntPtr dlsym(IntPtr handle, [MarshalAs(UnmanagedType.LPTStr)] string symbol);
+
+        [DllImport("libdl")]
+        static extern IntPtr dlopen(string filename, int flags);
+
+        [DllImport("__Internal")]
+        private static extern void mono_dllmap_insert(IntPtr assembly, IntPtr dll, IntPtr func, IntPtr tdll, IntPtr tfunc);
+
         public static void Load(string libname)
         {
             string subdir;
             string extension;
+            Func<string, IntPtr> Load;
 
             switch ((int)Environment.OSVersion.Platform)
             {
@@ -29,26 +39,45 @@ namespace RocksDbSharp
                 case (int)PlatformID.WinCE:
                     extension = ".dll";
                     subdir = Environment.Is64BitProcess ? "amd64" : "i386";
+                    Load = LoadLibrary;
                     break;
                 case (int)PlatformID.Unix:
                     extension = ".so";
                     subdir = Environment.Is64BitProcess ? "amd64" : "i386";
+                    Load = MonoLoad;
                     break;
                 case (int)PlatformID.MacOSX:
                 case 128: // Mono Mac
                     extension = ".dylib";
                     subdir = Environment.Is64BitProcess ? "amd64" : "i386";
+                    Load = MonoLoad;
                     break;
                 default:
                     extension = ".dll";
                     subdir = Environment.Is64BitProcess ? "amd64" : "i386";
+                    Load = LoadLibrary;
                     break;
             }
 
             var path = Path.Combine("native", subdir, libname + extension);
-            var handle = LoadLibrary(path);
+            var handle = Load(path);
+            /*
             if (handle == IntPtr.Zero)
                 throw new Exception(string.Format("Unable to load dll at {0}", path));
+            */
+        }
+
+        public static IntPtr MonoLoad(string path)
+        {
+            path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, AppDomain.CurrentDomain.RelativeSearchPath ?? "", path);
+
+            string name = Path.GetFileNameWithoutExtension(path);
+            IntPtr libraryNamePtr = Marshal.StringToHGlobalAnsi(name);
+            IntPtr pathPtr = Marshal.StringToHGlobalAnsi(path);
+            mono_dllmap_insert(IntPtr.Zero, libraryNamePtr, IntPtr.Zero, pathPtr, IntPtr.Zero);
+            Marshal.FreeHGlobal(libraryNamePtr);
+            Marshal.FreeHGlobal(pathPtr);
+            return IntPtr.Zero; //dlopen(path, 9);
         }
     }
 }
--- native-build/build-rocksdb.sh
+++ native-build/build-rocksdb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# This is designed to be able to be run from git bash
+# This is designed to be able to be run from git bash on Windows
 # You therefore should install git bash, Visual Studio 2015, and cmake
+# Your best bet in Windows is to open a Developer Command Prompt and then run bash from there.
 
 ROCKSDBVERSION=c6fedf2bf
 GFLAGSVERSION=9db82895
@@ -38,155 +39,178 @@ checkout() {
 	git checkout "$VERSION" || fail "Unable to checkout $NAME version ${VERSION}"
 }
 
-# Make sure git is installed
-hash git 2> /dev/null || { fail "Build requires git"; }
-test -z "$WindowsSdkDir" && fail "This must be run from a build environment such as the Developer Command Prompt"
-
 BASEDIR=$(dirname "$0")
-BASEDIRWIN=$(cd "${BASEDIR}" && pwd -W)
+OSINFO=$(uname)
 
-mkdir -p snappy || fail "unable to create snappy directory"
-(cd snappy && {
-	checkout "snappy" "$SNAPPYREMOTE" "$SNAPPYVERSION" "cmake"
-	mkdir -p build
-	(cd build && {
-		cmake -G "Visual Studio 14 2015 Win64" .. || fail "Running cmake on snappy failed"
-	}) || fail "cmake build generation failed"
-	test -z "$RUNTESTS" || {
-		cmd //c "msbuild build/snappy.sln /p:Configuration=Debug /m:$CONCURRENCY" || fail "Build of snappy (debug config) failed"
-	}
-	cmd //c "msbuild build/snappy.sln /p:Configuration=Release /m:$CONCURRENCY" || fail "Build of snappy failed"
-}) || fail "Snappy build failed"
+if [[ $OSINFO == *"MSYS"* ]]; then
+	echo "Detected Windows (MSYS)..."
+	# Make sure git is installed
+	hash git 2> /dev/null || { fail "Build requires git"; }
+	test -z "$WindowsSdkDir" && fail "This must be run from a build environment such as the Developer Command Prompt"
 
+	BASEDIRWIN=$(cd "${BASEDIR}" && pwd -W)
 
-mkdir -p gflags || fail "unable to create gflags directory"
-(cd gflags && {
-	checkout "gflags" "$GFLAGSREMOTE" "$GFLAGSVERSION" "master"
-	mkdir -p build
-	(cd build && {
-		cmake -G "Visual Studio 14 2015 Win64" .. || fail "Running cmake failed"
-	}) || fail "cmake build generation failed"
-	test -z "$RUNTESTS" || {
-		cmd //c "msbuild build/gflags.sln /p:Configuration=Debug /m:$CONCURRENCY" || fail "Build of gflags (debug config) failed"
-	}
-	cmd //c "msbuild build/gflags.sln /p:Configuration=Release /m:$CONCURRENCY" || fail "Build of gflags failed"
-}) || fail "GFlags build failed"
+	mkdir -p snappy || fail "unable to create snappy directory"
+	(cd snappy && {
+		checkout "snappy" "$SNAPPYREMOTE" "$SNAPPYVERSION" "cmake"
+		mkdir -p build
+		(cd build && {
+			cmake -G "Visual Studio 14 2015 Win64" .. || fail "Running cmake on snappy failed"
+		}) || fail "cmake build generation failed"
+		test -z "$RUNTESTS" || {
+			cmd //c "msbuild build/snappy.sln /p:Configuration=Debug /m:$CONCURRENCY" || fail "Build of snappy (debug config) failed"
+		}
+		cmd //c "msbuild build/snappy.sln /p:Configuration=Release /m:$CONCURRENCY" || fail "Build of snappy failed"
+	}) || fail "Snappy build failed"
 
 
-mkdir -p rocksdb || fail "unable to create rocksdb directory"
-(cd rocksdb && {
-	checkout "rocksdb" "$ROCKSDBREMOTE" "$ROCKSDBVERSION" "wf_win_master"
-	git checkout -- thirdparty.inc
-	patch -N < ../rocksdb.thirdparty.inc.patch || warn "Patching of thirdparty.inc failed"
-	rm -f thirdparty.inc.rej thirdparty.inc.orig
-	mkdir -p build
-	(cd build && {
-		cmake -G "Visual Studio 14 2015 Win64" -DOPTDBG=1 -DGFLAGS=1 -DSNAPPY=1 .. || fail "Running cmake failed"
-	}) || fail "cmake build generation failed"
+	mkdir -p gflags || fail "unable to create gflags directory"
+	(cd gflags && {
+		checkout "gflags" "$GFLAGSREMOTE" "$GFLAGSVERSION" "master"
+		mkdir -p build
+		(cd build && {
+			cmake -G "Visual Studio 14 2015 Win64" .. || fail "Running cmake failed"
+		}) || fail "cmake build generation failed"
+		test -z "$RUNTESTS" || {
+			cmd //c "msbuild build/gflags.sln /p:Configuration=Debug /m:$CONCURRENCY" || fail "Build of gflags (debug config) failed"
+		}
+		cmd //c "msbuild build/gflags.sln /p:Configuration=Release /m:$CONCURRENCY" || fail "Build of gflags failed"
+	}) || fail "GFlags build failed"
 
-	export TEST_TMPDIR=$(cmd //c "echo %TMP%")
 
-	# TODO: build debug version first and run tests
-	test -z "$RUNTESTS" || {
-		cmd //c "msbuild build/rocksdb.sln /p:Configuration=Debug /m:$CONCURRENCY" || fail "Rocksdb debug build failed"
-		run_rocksdb_test db_test
-		run_rocksdb_test db_iter_test
-		run_rocksdb_test db_log_iter_test
-		run_rocksdb_test db_compaction_filter_test
-		run_rocksdb_test db_compaction_test
-		run_rocksdb_test db_dynamic_level_test
-		run_rocksdb_test db_inplace_update_test
-		run_rocksdb_test db_tailing_iter_test
-		run_rocksdb_test db_universal_compaction_test
-		run_rocksdb_test db_wal_test
-		run_rocksdb_test db_table_properties_test
-		run_rocksdb_test block_hash_index_test
-		run_rocksdb_test autovector_test
-		run_rocksdb_test column_family_test
-		run_rocksdb_test table_properties_collector_test
-		run_rocksdb_test arena_test
-		run_rocksdb_test auto_roll_logger_test
-		run_rocksdb_test block_test
-		run_rocksdb_test bloom_test
-		run_rocksdb_test dynamic_bloom_test
-		run_rocksdb_test c_test
-		run_rocksdb_test cache_test
-		run_rocksdb_test checkpoint_test
-		run_rocksdb_test coding_test
-		run_rocksdb_test corruption_test
-		run_rocksdb_test crc32c_test
-		run_rocksdb_test slice_transform_test
-		run_rocksdb_test dbformat_test
-		run_rocksdb_test env_test
-		run_rocksdb_test fault_injection_test
-		run_rocksdb_test filelock_test
-		run_rocksdb_test filename_test
-		run_rocksdb_test file_reader_writer_test
-		run_rocksdb_test block_based_filter_block_test
-		run_rocksdb_test full_filter_block_test
-		run_rocksdb_test histogram_test
-		run_rocksdb_test inlineskiplist_test
-		run_rocksdb_test log_test
-		run_rocksdb_test manual_compaction_test
-		run_rocksdb_test memenv_test
-		run_rocksdb_test mock_env_test
-		run_rocksdb_test memtable_list_test
-		run_rocksdb_test merge_helper_test
-		run_rocksdb_test memory_test
-		run_rocksdb_test merge_test
-		run_rocksdb_test merger_test
-		run_rocksdb_test options_file_test
-		run_rocksdb_test redis_lists_test
-		run_rocksdb_test reduce_levels_test
-		run_rocksdb_test plain_table_db_test
-		run_rocksdb_test comparator_db_test
-		run_rocksdb_test prefix_test
-		run_rocksdb_test skiplist_test
-		run_rocksdb_test stringappend_test
-		run_rocksdb_test ttl_test
-		run_rocksdb_test backupable_db_test
-		run_rocksdb_test document_db_test
-		run_rocksdb_test json_document_test
-		run_rocksdb_test spatial_db_test
-		run_rocksdb_test version_edit_test
-		run_rocksdb_test version_set_test
-		run_rocksdb_test compaction_picker_test
-		run_rocksdb_test version_builder_test
-		run_rocksdb_test file_indexer_test
-		run_rocksdb_test write_batch_test
-		run_rocksdb_test write_batch_with_index_test
-		run_rocksdb_test write_controller_test
-		run_rocksdb_test deletefile_test
-		run_rocksdb_test table_test
-		run_rocksdb_test thread_local_test
-		run_rocksdb_test geodb_test
-		run_rocksdb_test rate_limiter_test
-		run_rocksdb_test delete_scheduler_test
-		run_rocksdb_test options_test
-		run_rocksdb_test options_util_test
-		run_rocksdb_test event_logger_test
-		run_rocksdb_test cuckoo_table_builder_test
-		run_rocksdb_test cuckoo_table_reader_test
-		run_rocksdb_test cuckoo_table_db_test
-		run_rocksdb_test flush_job_test
-		run_rocksdb_test wal_manager_test
-		run_rocksdb_test listener_test
-		run_rocksdb_test compaction_iterator_test
-		run_rocksdb_test compaction_job_test
-		run_rocksdb_test thread_list_test
-		run_rocksdb_test sst_dump_test
-		run_rocksdb_test compact_files_test
-		run_rocksdb_test perf_context_test
-		run_rocksdb_test optimistic_transaction_test
-		run_rocksdb_test write_callback_test
-		run_rocksdb_test heap_test
-		run_rocksdb_test compact_on_deletion_collector_test
-		run_rocksdb_test compaction_job_stats_test
-		run_rocksdb_test transaction_test
-		run_rocksdb_test ldb_cmd_test
-	}
-	cmd //c "msbuild build/rocksdb.sln /p:Configuration=Release /m:$CONCURRENCY" || fail "Rocksdb release build failed"
-	git checkout -- thirdparty.inc
-	mkdir -p ../../native/amd64 && cp -v ./build/Release/rocksdb.dll ../../native/amd64/librocksdb.dll
-}) || fail "rocksdb build failed"
+	mkdir -p rocksdb || fail "unable to create rocksdb directory"
+	(cd rocksdb && {
+		checkout "rocksdb" "$ROCKSDBREMOTE" "$ROCKSDBVERSION" "wf_win_master"
+		git checkout -- thirdparty.inc
+		patch -N < ../rocksdb.thirdparty.inc.patch || warn "Patching of thirdparty.inc failed"
+		rm -f thirdparty.inc.rej thirdparty.inc.orig
+		mkdir -p build
+		(cd build && {
+			cmake -G "Visual Studio 14 2015 Win64" -DOPTDBG=1 -DGFLAGS=1 -DSNAPPY=1 .. || fail "Running cmake failed"
+		}) || fail "cmake build generation failed"
 
+		export TEST_TMPDIR=$(cmd //c "echo %TMP%")
+
+		# TODO: build debug version first and run tests
+		test -z "$RUNTESTS" || {
+			cmd //c "msbuild build/rocksdb.sln /p:Configuration=Debug /m:$CONCURRENCY" || fail "Rocksdb debug build failed"
+			run_rocksdb_test db_test
+			run_rocksdb_test db_iter_test
+			run_rocksdb_test db_log_iter_test
+			run_rocksdb_test db_compaction_filter_test
+			run_rocksdb_test db_compaction_test
+			run_rocksdb_test db_dynamic_level_test
+			run_rocksdb_test db_inplace_update_test
+			run_rocksdb_test db_tailing_iter_test
+			run_rocksdb_test db_universal_compaction_test
+			run_rocksdb_test db_wal_test
+			run_rocksdb_test db_table_properties_test
+			run_rocksdb_test block_hash_index_test
+			run_rocksdb_test autovector_test
+			run_rocksdb_test column_family_test
+			run_rocksdb_test table_properties_collector_test
+			run_rocksdb_test arena_test
+			run_rocksdb_test auto_roll_logger_test
+			run_rocksdb_test block_test
+			run_rocksdb_test bloom_test
+			run_rocksdb_test dynamic_bloom_test
+			run_rocksdb_test c_test
+			run_rocksdb_test cache_test
+			run_rocksdb_test checkpoint_test
+			run_rocksdb_test coding_test
+			run_rocksdb_test corruption_test
+			run_rocksdb_test crc32c_test
+			run_rocksdb_test slice_transform_test
+			run_rocksdb_test dbformat_test
+			run_rocksdb_test env_test
+			run_rocksdb_test fault_injection_test
+			run_rocksdb_test filelock_test
+			run_rocksdb_test filename_test
+			run_rocksdb_test file_reader_writer_test
+			run_rocksdb_test block_based_filter_block_test
+			run_rocksdb_test full_filter_block_test
+			run_rocksdb_test histogram_test
+			run_rocksdb_test inlineskiplist_test
+			run_rocksdb_test log_test
+			run_rocksdb_test manual_compaction_test
+			run_rocksdb_test memenv_test
+			run_rocksdb_test mock_env_test
+			run_rocksdb_test memtable_list_test
+			run_rocksdb_test merge_helper_test
+			run_rocksdb_test memory_test
+			run_rocksdb_test merge_test
+			run_rocksdb_test merger_test
+			run_rocksdb_test options_file_test
+			run_rocksdb_test redis_lists_test
+			run_rocksdb_test reduce_levels_test
+			run_rocksdb_test plain_table_db_test
+			run_rocksdb_test comparator_db_test
+			run_rocksdb_test prefix_test
+			run_rocksdb_test skiplist_test
+			run_rocksdb_test stringappend_test
+			run_rocksdb_test ttl_test
+			run_rocksdb_test backupable_db_test
+			run_rocksdb_test document_db_test
+			run_rocksdb_test json_document_test
+			run_rocksdb_test spatial_db_test
+			run_rocksdb_test version_edit_test
+			run_rocksdb_test version_set_test
+			run_rocksdb_test compaction_picker_test
+			run_rocksdb_test version_builder_test
+			run_rocksdb_test file_indexer_test
+			run_rocksdb_test write_batch_test
+			run_rocksdb_test write_batch_with_index_test
+			run_rocksdb_test write_controller_test
+			run_rocksdb_test deletefile_test
+			run_rocksdb_test table_test
+			run_rocksdb_test thread_local_test
+			run_rocksdb_test geodb_test
+			run_rocksdb_test rate_limiter_test
+			run_rocksdb_test delete_scheduler_test
+			run_rocksdb_test options_test
+			run_rocksdb_test options_util_test
+			run_rocksdb_test event_logger_test
+			run_rocksdb_test cuckoo_table_builder_test
+			run_rocksdb_test cuckoo_table_reader_test
+			run_rocksdb_test cuckoo_table_db_test
+			run_rocksdb_test flush_job_test
+			run_rocksdb_test wal_manager_test
+			run_rocksdb_test listener_test
+			run_rocksdb_test compaction_iterator_test
+			run_rocksdb_test compaction_job_test
+			run_rocksdb_test thread_list_test
+			run_rocksdb_test sst_dump_test
+			run_rocksdb_test compact_files_test
+			run_rocksdb_test perf_context_test
+			run_rocksdb_test optimistic_transaction_test
+			run_rocksdb_test write_callback_test
+			run_rocksdb_test heap_test
+			run_rocksdb_test compact_on_deletion_collector_test
+			run_rocksdb_test compaction_job_stats_test
+			run_rocksdb_test transaction_test
+			run_rocksdb_test ldb_cmd_test
+		}
+		cmd //c "msbuild build/rocksdb.sln /p:Configuration=Release /m:$CONCURRENCY" || fail "Rocksdb release build failed"
+		git checkout -- thirdparty.inc
+		mkdir -p ../../native/amd64 && cp -v ./build/Release/rocksdb.dll ../../native/amd64/librocksdb.dll
+	}) || fail "rocksdb build failed"
+elif [[ $OSDETECT == *"Darwin"* ]]; then
+	fail "Mac OSX build is not yet operational"
+else
+	echo "Assuming a linux-like environment"
+
+	mkdir -p rocksdb || fail "unable to create rocksdb directory"
+	(cd rocksdb && {
+		checkout "rocksdb" "$ROCKSDBREMOTE" "$ROCKSDBVERSION" "wf_win_master"
+
+		(. ./build_tools/build_detect_platform detected~; {
+			grep detected~ -e '-DSNAPPY' &> /dev/null || fail "failed to detect snappy, install libsnappy-dev"
+			grep detected~ -e '-DGFLAGS' &> /dev/null || fail "failed to detect gflags, install libgflags-dev"
+			grep detected~ -e '-DZLIB' &> /dev/null || fail "failed to detect zlib, install libzlib-dev"
+		}) || fail "dependency detection failed"
+
+		make -j$CONCURRENCY shared_lib
+		strip librocksdb.so.4.3.0
+		mkdir -p ../../native/amd64 && cp -v ./librocksdb.so.4.3.0 ../../native/amd64/librocksdb.so
+	}) || fail "rocksdb build failed"
+fi
