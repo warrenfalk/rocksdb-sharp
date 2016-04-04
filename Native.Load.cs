bfc6b25c8b2d9e4d693385baf2132db5c37293d6
Warren Falk <warren@warrenfalk.com>
2016-04-04 05:47:11 -0400

    throw a more friendly error if running in 32 bit windows process

--- Native.Load.cs
+++ Native.Load.cs
@@ -8,6 +8,14 @@ namespace RocksDbSharp
 {
     public abstract partial class Native
     {
-        public static Native Instance = NativeImport.Auto.Import<Native>("librocksdb", true);
+        public static Native Instance;
+        
+        static Native()
+        {
+            var subdir = Environment.Is64BitProcess ? "amd64" : "i386";
+            if (!Environment.Is64BitProcess && (int)Environment.OSVersion.Platform <= 3)
+                throw new RocksDbSharpException("Rocksdb on windows is not supported for 32 bit applications");
+            Instance = NativeImport.Auto.Import<Native>("librocksdb", true);
+        }
     }
 }
