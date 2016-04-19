30a5a5fac88955f09f71821807b5a355f23b7cc2
Warren Falk <warren@warrenfalk.com>
2016-04-19 18:10:02 -0400

    update prefix example somewhat and make explicitly 64 bit

--- examples/PrefixExample/Program.cs
+++ examples/PrefixExample/Program.cs
@@ -21,19 +21,54 @@ namespace PrefixExample
             string path = Environment.ExpandEnvironmentVariables(Path.Combine(temp, "rocksdb_prefix_example"));
             var bbto = new BlockBasedTableOptions()
                 .SetFilterPolicy(BloomFilterPolicy.Create(10, false))
-                .SetWholeKeyFiltering(false);
+                .SetWholeKeyFiltering(false)
+                ;
             var options = new DbOptions()
                 .SetCreateIfMissing(true)
-                .SetWriteBufferSize(writeBufferSize)
-                .SetMaxWriteBufferNumber(maxWriteBufferNumber)
-                .SetMinWriteBufferNumberToMerge(minWriteBufferNumberToMerge)
-                .SetMemtablePrefixBloomBits(memtablePrefixBloomBits)
-                .SetMemtablePrefixBloomProbes(memtablePrefixBloomProbes)
-                .SetMemtablePrefixBloomHugePageTlbSize(memtablePrefixBloomHugePageTlbSize)
-                .SetPrefixExtractor(SliceTransform.CreateFixedPrefix(8))
-                .SetBlockBasedTableFactory(bbto);
-            using (var db = RocksDb.Open(options, path))
+                .SetCreateMissingColumnFamilies(true)
+                ;
+            var columnFamilies = new ColumnFamilies
             {
+                { "default", new ColumnFamilyOptions().OptimizeForPointLookup(256) },
+                { "test", new ColumnFamilyOptions()
+                    //.SetWriteBufferSize(writeBufferSize)
+                    //.SetMaxWriteBufferNumber(maxWriteBufferNumber)
+                    //.SetMinWriteBufferNumberToMerge(minWriteBufferNumberToMerge)
+                    .SetMemtablePrefixBloomBits(10000000)
+                    .SetMemtablePrefixBloomProbes(10)
+                    .SetMemtablePrefixBloomHugePageTlbSize(2 * 1024 * 1024)
+                    .SetPrefixExtractor(SliceTransform.CreateFixedPrefix((ulong)8))
+                    .SetBlockBasedTableFactory(bbto)
+                },
+            };
+            using (var db = RocksDb.Open(options, path, columnFamilies))
+            {
+                var cf = db.GetColumnFamily("test");
+
+                db.Put("00000000Zero", "", cf: cf);
+                db.Put("00000000One", "", cf: cf);
+                db.Put("00000000Two", "", cf: cf);
+                db.Put("00000000Three", "", cf: cf);
+                db.Put("00000001Red", "", cf: cf);
+                db.Put("00000001Green", "", cf: cf);
+                db.Put("00000001Black", "", cf: cf);
+                db.Put("00000002Apple", "", cf: cf);
+                db.Put("00000002Cranberry", "", cf: cf);
+                db.Put("00000002Banana", "", cf: cf);
+
+                var readOptions = new ReadOptions();
+                using (var iter = db.NewIterator(readOptions: readOptions, cf: cf))
+                {
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+                    var b = Encoding.UTF8.GetBytes("00000001");
+                    iter.Seek(b);
+                    while (iter.Valid())
+                    {
+                        Console.WriteLine(iter.StringKey());
+                        iter.Next();
+                    }
+                }
             }
             Console.WriteLine("Done...");
         }
