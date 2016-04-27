bcf64854c0839a0377f347b2b9d6e1c9e64d1ef7
Warren Falk <warren@warrenfalk.com>
2016-04-27 10:38:12 -0400

    fix directory structure in binary download script

--- download-native.sh
+++ download-native.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 [ -f native.zip ] || curl -L https://github.com/warrenfalk/rocksdb-sharp/releases/download/v0.4.5-alpha/native-d733dd572.zip --output native.zip
-[ -f native.zip ] && unzip native.zip
+[ -f native.zip ] && { mkdir -p native; cd native && unzip ../native.zip; }
 [ -f native.zip -a -f native/amd64/librocksdb.so ] && rm native.zip
 
