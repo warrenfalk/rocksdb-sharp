d67258374073c544a72107dda51b6623e5b459be
Warren Falk <warren@warrenfalk.com>
2016-08-23 20:50:45 -0400

    fixup update to 4.9

--- download-native.sh
+++ download-native.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-[ -f native.zip ] || curl -L https://github.com/warrenfalk/rocksdb-sharp/releases/download/v0.4.9/native-e70aabc3.zip --output native.zip
+[ -f native.zip ] || curl -L https://github.com/warrenfalk/rocksdb-sharp/releases/download/v4.9.0/native-e70aabc3.zip --output native.zip
 [ -f native.zip ] && { mkdir -p native; cd native && unzip ../native.zip; }
 [ -f native.zip -a -f native/amd64/librocksdb.so ] && rm native.zip
 
