5efdc50c7cee588498975dbaae2ac95d4ca91108
Warren Falk <warren@warrenfalk.com>
2016-12-17 12:51:56 -0500

    version to 4.13.1

--- download-native.sh
+++ download-native.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-[ -f native.zip ] || curl -L https://github.com/warrenfalk/rocksdb-sharp/releases/download/v4.13.0/native-0a49cbee.zip --output native.zip
+[ -f native.zip ] || curl -L https://github.com/warrenfalk/rocksdb-sharp/releases/download/v4.13.1/native-0a49cbee.zip --output native.zip
 [ -f native.zip ] && { mkdir -p native; cd native && unzip ../native.zip; }
 [ -f native.zip -a -f native/amd64/librocksdb.so ] && rm native.zip
 
