e06992b2fc1aeb492831c01b3de2d05d9b2990ff
Warren Falk <warren@warrenfalk.com>
2017-04-25 08:06:43 -0400

    add a PrefixEquals to binary comparer

--- shared/BinaryComparer.cs
+++ shared/BinaryComparer.cs
@@ -52,6 +52,31 @@ namespace RocksDbSharp
             }
         }
 
+        public bool PrefixEquals(byte[] a1, byte[] a2, int prefix)
+        {
+            if (ReferenceEquals(a1, a2))
+                return true;
+            prefix = Math.Min(prefix, Math.Max(a1.Length, a2.Length));
+            var a1length = Math.Min(prefix, a1.Length);
+            var a2length = Math.Min(prefix, a2.Length);
+            if (a1 == null || a2 == null || a1length != a2length)
+                return false;
+            unsafe
+            {
+                fixed (byte* p1 = a1, p2 = a2)
+                {
+                    byte* x1 = p1, x2 = p2;
+                    int l = a1length;
+                    for (int i = 0; i < l / 8; i++, x1 += 8, x2 += 8)
+                        if (*((long*)x1) != *((long*)x2)) return false;
+                    if ((l & 4) != 0) { if (*((int*)x1) != *((int*)x2)) return false; x1 += 4; x2 += 4; }
+                    if ((l & 2) != 0) { if (*((short*)x1) != *((short*)x2)) return false; x1 += 2; x2 += 2; }
+                    if ((l & 1) != 0) if (*((byte*)x1) != *((byte*)x2)) return false;
+                    return true;
+                }
+            }
+        }
+
         public int GetHashCode(byte[] obj)
         {
             return MurMurHash3.Hash(new MemoryStream(obj));
--- tests/RocksDbSharpTest/TestBinaryComparer.cs
+++ tests/RocksDbSharpTest/TestBinaryComparer.cs
@@ -29,6 +29,34 @@ namespace RocksDbSharpTest
             AssertCompare(comparer, backward, "bb", "b");
         }
 
+        [TestMethod]
+        public void TestPrefixEquals()
+        {
+            var comparer = BinaryComparer.Default;
+
+            Assert.IsTrue(comparer.PrefixEquals(AsciiBytes("aaa"), AsciiBytes("aaa"), 1));
+            Assert.IsTrue(comparer.PrefixEquals(AsciiBytes("aaa"), AsciiBytes("aaa"), 3));
+            Assert.IsTrue(comparer.PrefixEquals(AsciiBytes("aaa"), AsciiBytes("aaa"), 5));
+
+            Assert.IsTrue(comparer.PrefixEquals(AsciiBytes("aaa"), AsciiBytes("aaX"), 1));
+            Assert.IsTrue(comparer.PrefixEquals(AsciiBytes("aaa"), AsciiBytes("aaX"), 2));
+            Assert.IsFalse(comparer.PrefixEquals(AsciiBytes("aaa"), AsciiBytes("aaX"), 3));
+            Assert.IsFalse(comparer.PrefixEquals(AsciiBytes("aaa"), AsciiBytes("aaX"), 5));
+
+            Assert.IsTrue(comparer.PrefixEquals(AsciiBytes("aaa"), AsciiBytes("aaaX"), 1));
+            Assert.IsTrue(comparer.PrefixEquals(AsciiBytes("aaa"), AsciiBytes("aaaX"), 3));
+            Assert.IsFalse(comparer.PrefixEquals(AsciiBytes("aaa"), AsciiBytes("aaaX"), 4));
+
+            Assert.IsTrue(comparer.PrefixEquals(AsciiBytes("aaaX"), AsciiBytes("aaa"), 1));
+            Assert.IsTrue(comparer.PrefixEquals(AsciiBytes("aaaX"), AsciiBytes("aaa"), 3));
+            Assert.IsFalse(comparer.PrefixEquals(AsciiBytes("aaaX"), AsciiBytes("aaa"), 4));
+        }
+
+        private byte[] AsciiBytes(string v)
+        {
+            return Encoding.ASCII.GetBytes(v);
+        }
+
         private void AssertCompare(BinaryComparer comparer, int expected, string v1, string v2)
         {
             Assert.AreEqual(expected, comparer.Compare(Encoding.UTF8.GetBytes(v1), Encoding.UTF8.GetBytes(v2)), string.Format("{0} -> {1}", v1, v2));
