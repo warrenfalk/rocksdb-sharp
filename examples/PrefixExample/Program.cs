005877b46e865e0e3b010e25ed88c473eb0abd50
Warren Falk <warren@warrenfalk.com>
2017-04-25 12:59:14 -0400

    eliminate some warnings

--- examples/PrefixExample/Program.cs
+++ examples/PrefixExample/Program.cs
@@ -10,13 +10,6 @@ namespace PrefixExample
     {
         static void Main(string[] args)
         {
-            ulong writeBufferSize = 33554432;
-            int maxWriteBufferNumber = 2;
-            int minWriteBufferNumberToMerge = 1;
-            uint memtablePrefixBloomBits = 10000000;
-            int memtablePrefixBloomProbes = 10;
-            ulong memtablePrefixBloomHugePageTlbSize = 2 * 1024 * 1024;
-            
             string temp = Path.GetTempPath();
             string path = Environment.ExpandEnvironmentVariables(Path.Combine(temp, "rocksdb_prefix_example"));
             var bbto = new BlockBasedTableOptions()
