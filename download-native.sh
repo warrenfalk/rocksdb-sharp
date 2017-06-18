a410e7c3bf41541c5410946f81ccfa24d4820314
Warren Falk <warren@warrenfalk.com>
2017-06-18 11:07:08 -0400

    RocksDbNative to 5.4.6.2

--- download-native.sh
+++ download-native.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-[ -f native.zip ] || curl -L https://github.com/warrenfalk/rocksdb-sharp/releases/download/v5.4.6/native-6e05979.zip --output native.zip
+[ -f native.zip ] || curl -L https://github.com/warrenfalk/rocksdb-sharp/releases/download/v5.4.6.2/native-6e05979.zip --output native.zip
 [ -f native.zip ] && { mkdir -p native; cd native && unzip ../native.zip; }
 [ -f native.zip -a -f native/amd64/librocksdb.so ] && rm native.zip
 
