832195a61779c795a6c2e06e513a4c285ff6abcb
Gilad Levi <giladl@wk-giladl.eaglelabs.group>
2020-01-29 16:45:11 +0200

    for packing win/linux nuget

--- RocksDbSharp/Native.Load.cs
+++ RocksDbSharp/Native.Load.cs
@@ -15,7 +15,7 @@ namespace RocksDbSharp
         {
             if (RuntimeInformation.ProcessArchitecture == Architecture.X86 && RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                 throw new RocksDbSharpException("Rocksdb on windows is not supported for 32 bit applications");
-            Instance = NativeImport.Auto.Import<Native>("rocksdb", "6.2.2", true);
+            Instance = NativeImport.Auto.Import<Native>("rocksdb", "6.6.3", true);
         }
 
         public Native()
